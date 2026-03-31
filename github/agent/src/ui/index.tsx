51e3676b5403a4cfa139cc8b3cc51e6095752a14
iwh-brianw <brianw@interworldhighway.com>
2026-03-31 11:17:23 -0700

    fixed github name

--- github/agent/src/ui/index.tsx
+++
@@ -1,5 +0,0 @@
-export { App } from './App.tsx';
-export { MessageList, type Message } from './components/MessageList.tsx';
-export { ToolCall, type ToolCallProps } from './components/ToolCall.tsx';
-export { Spinner } from './components/Spinner.tsx';
-export { Input } from './components/Input.tsx';
